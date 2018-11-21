0042db39dfb1f5590de1dcd37504c61a2404fae4
Renato Golia <renato.golia@educations.com>
2018-11-21 13:35:39 +0100

    Support for serialized view model

--- FirstSpecimen/WebApp/ClientApp/src/app/users/view-user-albums/view-user-albums.component.ts
+++ FirstSpecimen/WebApp/ClientApp/src/app/users/view-user-albums/view-user-albums.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'view-user-albums',
@@ -11,21 +10,21 @@ export class ViewUserAlbumsComponent implements OnInit {
 
   albums : Album[];
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    let userId = this.route.snapshot.params.id;
+    let userId = viewModel.User.Id;
     let url = `https://jsonplaceholder.typicode.com/users/${userId}/albums`;
-    
-    
 
     this.http.get<Album[]>(url).subscribe(items => this.albums = items);  
   }
 
 }
 
-export interface Album{
+export interface Album {
   id: number;
   title: string;
   userId: number;
 }
+
+declare var viewModel : any;
--- FirstSpecimen/WebApp/ClientApp/src/app/users/view-user-posts/view-user-posts.component.ts
+++ FirstSpecimen/WebApp/ClientApp/src/app/users/view-user-posts/view-user-posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'view-user-posts',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewUserPostsComponent implements OnInit {
 
-  constructor() { }
+  posts : Post[];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-  }
+    let userId = viewModel.User.Id;
+    let url = `https://jsonplaceholder.typicode.com/users/${userId}/posts`;
 
+    this.http.get<Post[]>(url).subscribe(items => this.posts = items);  
+  }
 }
+
+export interface Post{
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
+
+declare var viewModel : any;
